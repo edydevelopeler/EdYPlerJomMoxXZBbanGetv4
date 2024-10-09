092dde5dc456fa4770ad630185409d569bb41bf0
Edy Dev <152673375+edydevelopeler@users.noreply.github.com>
2024-10-09 12:30:46 +0700

    ngentot lu

--- files/cf.sh
+++ files/cf.sh
@@ -9,10 +9,10 @@ echo ""
 echo ""
 #sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
 read -rp "Input Domain Name. Example ( Namamu21 ): " -e sub
-DOMAIN=serverpremium.biz.id
-SUB_DOMAIN=${sub}.serverpremium.biz.id
-CF_ID=vscobangst111@gmail.com
-CF_KEY=4a912a2d56a1b3837d48751da5334b74a4fb8
+DOMAIN=jomox.id
+SUB_DOMAIN=${sub}.jomox.id
+CF_ID=lunoxximpostor@gmail.com
+CF_KEY=e53921e0a2e43294f50a48d68bd1077ce3037
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
